ef0fe916c9818c89819d6d7844e48f5761048e5a
Nayoung Kong <ahenaka9@gmail.com>
2021-09-09 20:18:53 +0900

    Update world.py

--- world.py
+++ world.py
@@ -5,10 +5,10 @@ def hello_world():
     print("hello world")
 
 def bye_world():
+    time.sleep(2)
     print("bye world")
 
 if __name__ == "__main__":
     hello_world()
-    time.sleep(2)
     bye_world()
 
